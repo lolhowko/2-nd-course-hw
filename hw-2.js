e52fd673384889898198d9c5c32b050975601956
Lana <lolhowko@gmail.com>
2023-06-26 21:57:00 +0300

    added file hw-2 JS

---
+++ hw-2.js
@@ -0,0 +1,104 @@
+//ЗАДАНИЕ10
+
+let userName = prompt("Введите Ваше имя, пожалуйста.");
+alert(`Привет, ${userName} !`);
+
+
+//ЗАДАНИЕ9.0
+
+let user = {
+    name: 'Lana',
+    age: 26,
+    isAdmin: true,
+}
+
+//ЗАДАНИЕ9.1
+
+user['cityofresidence'] = 'Moscow';
+
+//ЗАДАНИЕ9.2
+
+user.age = 18;
+
+//ЗАДАНИЕ9.3
+delete user['cityofresidence'];
+
+//ЗАДАНИЕ9.4
+
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+console.log(user[info]);
+alert(user);
+
+
+//ЗАДАНИЕ8
+
+user.age = prompt("Сколько Вам лет?");
+alert(age);
+
+
+//ЗАДАНИЕ7
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+
+//ЗАДАНИЕ6
+
+let a = 9;
+let b = 2;
+let result = a%b;
+
+alert(`остаток от деления a на b = ${result}.`)
+
+//ЗАДАНИЕ5
+
+let result = 2**5;
+alert(`2 в 5ой степени = ${result}.`);
+
+//ЗАДАНИЕ4
+
+let numberOne = 10;
+let numberTwo = 2;
+
+//сумма:
+let sum = numberOne + numberTwo;
+alert(sum);
+
+//разность:
+let difference = numberOne - numberTwo;
+alert(difference);
+
+//произведение (умножение):
+let multiplication = numberOne * numberTwo;
+alert(multiplication);
+
+//частное (деление):
+let division = numberOne / numberTwo;
+alert(division);
+
+//ЗАДАНИЕ3
+
+let creatorJS;
+creatorJS = "Брендан Айк";
+alert(`Создателем JavaScript является - ${creatorJS} `);
+
+//ЗАДАНИЕ2
+
+let releaseIphone;
+releaseIphone = 2009;
+alert(`Год выпуска первого iPone - ${releaseIphone} год`);
+
+//ЗАДАНИЕ1
+
+let a;
+a = 10;
+alert(a);
+
+a = 20;
+alert(a = 20);
