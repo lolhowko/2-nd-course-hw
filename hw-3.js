c09ddb969fd583680677e19c88489c7df1ea89d6
Lana <lolhowko@gmail.com>
2023-06-27 17:29:34 +0300

    Added hw-3 and 1-5 tasks

---
+++ hw-3.js
@@ -0,0 +1,90 @@
+
+//ЗАДАНИЕ 5
+let monthNumber = Number(prompt('Введите номер месяца'));
+
+switch (monthNumber) {
+    case Number("1"):
+        console.log('Зима');
+        break;
+    case Number("2"):
+        console.log('Зима');
+        break;
+    case Number("3"):
+        console.log('Весна');
+        break;
+    case Number("4"):
+        console.log('Весна');
+        break;
+    case Number("5"):
+        console.log('Весна');
+        break;
+    case Number("6"):
+        console.log('Лето');
+        break;
+    case Number("7"):
+        console.log('Лето');
+        break;
+    case Number("8"):
+        console.log('Лето');
+        break;
+    case Number("9"):
+        console.log('Осень');
+        break;
+    case Number("10"):
+        console.log('Осень');
+        break;
+    case Number("11"):
+        console.log('Осень');
+        break;
+    case Number("12"):
+        console.log('Зима');
+        break;
+    default:
+        console.log('Такого месяца нет');
+        break;
+}
+
+/*
+//ЗАДАНИЕ 4
+
+let a = '2';
+let b = '3';
+// Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
+alert(Number(a) + Number(b));
+
+
+//ЗАДАНИЕ 3
+let d = 10000;
+let e = 10;
+
+if (d > 100 || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+
+//ЗАДАНИЕ 2
+
+//let c = Number(prompt('Введите число'));
+let c = 10;
+
+if (c > '0' && c < '10') {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+
+//ЗАДАНИЕ 1
+
+let password = 'пароль';
+let enterPassworg = prompt('Введите пароль');
+
+
+if (password === 'пароль') {
+    alert("Пароль введен верно");
+} else {
+    alert("Пароль введен неправильно");
+}
+*/
