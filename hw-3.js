e257199995d934e8762286c7ff3db9f1fa7e3be0
Lana <lolhowko@gmail.com>
2023-07-03 20:25:42 +0300

    added corrections to hw.3

--- hw-3.js
+++ hw-3.js
@@ -51,35 +51,21 @@ let monthNumber = Number(prompt('Введите номер месяца'));
 
 switch (monthNumber) {
     case Number("1"):
-        console.log('Зима');
-        break;
     case Number("2"):
         console.log('Зима');
         break;
     case Number("3"):
-        console.log('Весна');
-        break;
     case Number("4"):
-        console.log('Весна');
-        break;
     case Number("5"):
         console.log('Весна');
         break;
     case Number("6"):
-        console.log('Лето');
-        break;
     case Number("7"):
-        console.log('Лето');
-        break;
     case Number("8"):
         console.log('Лето');
         break;
     case Number("9"):
-        console.log('Осень');
-        break;
     case Number("10"):
-        console.log('Осень');
-        break;
     case Number("11"):
         console.log('Осень');
         break;
@@ -116,7 +102,7 @@ if (d > 100 || e > 100) {
 //let c = Number(prompt('Введите число'));
 let c = 10;
 
-if (c > '0' && c < '10') {
+if (c > 0 && c < 10) {
     console.log('Верно');
 } else {
     console.log('Неверно');
